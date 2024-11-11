4ad199ab1dc2113c7c539ba31d997d4411f78fb9
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2024-11-11 15:36:26 -0500

    add all initial files

---
+++ MBAACC-Shock-Collar/MBAACC-Shock-Collar.cpp
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <iostream>
+
+// make sure to run vcpkg install --triplet x86-windows
+#include <curl/curl.h>
+
+static_assert(sizeof(void*) == 4, "program must be compiled in 32 bit mode");
+
+void sendShock() {
+
+
+
+}
+
+int main() {
+
+	printf("omfg\n");
+ 
+    while (true) {
+
+       
+        Sleep(100);
+    };
+
+
+	return 0;
+}
+
